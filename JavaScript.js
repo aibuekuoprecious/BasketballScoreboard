67118ed903138a2a04c219386029e1d22f2200f5
Aibueku Precious <aibuekuprecious@gmail.com>
2022-07-02 03:01:12 +0100

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,29 @@
+﻿let home = 0;
+let guest = 0;
+document.getElementById("homeSheet").textContent = home;
+document.getElementById("guestSheet").textContent = guest;
+
+function hAdd1() {
+        home += 1;
+        document.getElementById("homeSheet").textContent = home;
+}
+function hAdd2() {
+    home += 2;
+    document.getElementById("homeSheet").textContent = home;
+}
+function hAdd3() {
+    home += 3;
+    document.getElementById("homeSheet").textContent = home;
+}
+function gAdd1() {
+    guest += 1;
+    document.getElementById("guestSheet").textContent = guest;
+}
+function gAdd2() {
+    guest += 2;
+    document.getElementById("guestSheet").textContent = guest;
+}
+function gAdd3() {
+    guest += 3;
+    document.getElementById("guestSheet").textContent = guest;
+}
